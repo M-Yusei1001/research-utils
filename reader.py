20f0655eb66005d22d01b1b344c83137b44a4723
M-Yusei1001 <bitterchocolate1001@gmail.com>
2024-10-17 12:15:39 +0900

    csv読み書き

--- reader.py
+++ reader.py
@@ -1,14 +1,24 @@
 import csv
-import pprint
+import pandas as pd
+import tqdm
 
+output_filename = "incident_data_processed.csv"
+
+#データ整形
 def toRegExp(data:str) -> str:
-    return data.replace("\u3000", "")
+    data = data.replace("\u3000", "")
+    data = data.replace("\ufeff", "")
+    return data
 
-with open("data/src/J20241017103313.csv", encoding="utf-8") as f:
-    #csv読み込み
-    reader = csv.reader(f)
+#生データcsv読み込み
+with open("data/src/J20241017103313.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file)
     
     #読み込んだデータを2次元配列に変換、整形
     l = [[toRegExp(elem) for elem in row] for row in reader]
 
-pprint.pprint(l[1])
+#整形データ書き出し
+with open(f"data/output/{output_filename}", "w", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    for row in tqdm.tqdm(l):
+        writer.writerow(row)
